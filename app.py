75ff4c9f965c442bbf6c6c3a5a49ddd4fde0c04b
avishash108-hub <avishash108@gmail.com>
2026-01-18 01:55:44 +0530

    bot generating replies after webhook route being established
    Added functionality to send a reply message to Telegram users based on incoming webhook data.

--- app.py
+++ app.py
@@ -8,5 +8,20 @@ def home():
 @app.route("/webhook", methods = ["POST"])
 def telegram_webhook():
     data = request.get_json()
-    print(data)
+    chat_id = data["message"]["chat"]["id"]
+    text = data["message"].get("text", "")
+
+    reply = "Hello 👋 I am Awaaz. I'm here to help you."
+    send_message(chat_id, reply)
     return "OK", 200
+
+    
+def send_messages(chat_id, text):
+    url = f"https://api.telegram.org/bot7673865028:AAE4uebFECz0y7Gzg_7tB1KWLNzcPwKBw4g/sendMessage"
+    payload = {
+        "chat_id" : chat_id
+        "text" : text
+    }
+    requests.post(url, json=payload)
+    
+

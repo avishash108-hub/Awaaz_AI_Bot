50d5af9912dc34a08c8ff56a2aefd6b571dad796
avishash108-hub <avishash108@gmail.com>
2026-01-22 02:35:11 +0530

    Refactor Telegram webhook and add credentials handling

--- app.py
+++ app.py
@@ -2,15 +2,25 @@ from flask import Flask, request
 import os
 import requests
 from google.cloud import dialogflow_v2 as dialogflow
+import json
+from google.oauth2 import service_account
+credentials_info = json.loads(
+    os.environ.get("GOOGLE_APPLICATION_CREDENTIALS_JSON")
+)
+
+credentials = service_account.Credentials.from_service_account_info(
+    credentials_info
+)
 
 app = Flask(__name__)
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 PROJECT_ID = os.environ.get("DIALOGFLOW_PROJECT_ID")
 
-def detect_intent_texts(project_id, session_id, text, language_code="en"):
-    session_client = dialogflow.SessionsClient()
+def detect_intent_texts(text, session_id, language_code="en"):
+    session_client = dialogflow.SessionsClient(credentials=credentials)
 
-    session = session_client.session_path(project_id, session_id)
+
+    session = session_client.session_path(PROJECT_ID , session_id)
 
     text_input = dialogflow.TextInput(
         text=text,
@@ -31,22 +41,21 @@ def detect_intent_texts(project_id, session_id, text, language_code="en"):
 @app.route("/")
 def home():
     return "Awaaz AI backend running on Render"
-@app.route("/webhook", methods = ["POST"])
+@app.route("/webhook", methods=["POST"])
 def telegram_webhook():
     data = request.get_json()
+
+    if "message" not in data:
+        return "", 200
+
     chat_id = data["message"]["chat"]["id"]
     text_received = data["message"].get("text", "")
-     
 
-session_id = str(chat_id)
-
-reply = detect_intent_texts(
-    PROJECT_ID,
-    session_id,
-    user_text
-)
+    reply = detect_intent_texts(text_received, str(chat_id))
     send_message(chat_id, reply)
-    return "OK", 200
+
+    return "", 200
+
 
     
 def send_message(chat_id, reply):
@@ -57,6 +66,7 @@ def send_message(chat_id, reply):
     }
     requests.post(url, json=payload)
     
+
 
 
 
